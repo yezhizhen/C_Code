3b6e262c1fe06bb263ba3c0a2b12a242cf05cc89
yezhizhen <yezhizhenjiakang@gmail.com>
2015-09-24 08:51:52 +0800

    sigaction process

---
+++ lecture_code/OS/assignment1/sigaction.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void sighandler(int);
+
+int main()
+{
+	char* oper=malloc(sizeof(*oper)*11);
+	struct sigaction oldact;
+	struct sigaction newact;
+	newact.sa_handler = sighandler;
+	sigset_t nm = newact.sa_mask;
+	sigemptyset(&nm);
+	if(sigaction(SIGINT,NULL,&oldact)==-1)
+	{
+		perror("error...\n");
+	}	
+	else
+	{
+		if(oldact.sa_handler==SIG_DFL)
+		{
+			printf("original is signal default mechanism\n");
+		}
+		else if(oldact.sa_handler==SIG_IGN)
+		{
+
+			printf("originally signal is ignored\n");
+		}
+	}
+
+	if(sigaction(SIGINT,&newact,NULL)==-1)
+	{
+		perror("error to set new sigaction\n");
+	}
+	if(sigaction(SIGQUIT,&newact,NULL)==-1)
+	{
+		perror("error to set new sigaction\n");
+	}
+	while(1)
+	{
+		if(strcmp(oper,"block")==0)
+		{
+			sigaddset(&nm,SIGINT);
+			sigprocmask(SIG_BLOCK,&nm,NULL);
+			
+		}
+		sleep(1);
+		printf("sleeping....\n");
+		scanf("%10s",oper);
+		
+
+
+	}
+	
+
+}
+
+void sighandler(int signum)
+{
+	printf("Caught signal %d, coming out...\n", signum);
+}

f1b399193f9ef7fd1db06e07577237f5deb6c4ed
yezhizhen <yezhizhenjiakang@gmail.com>
2015-09-29 13:08:11 +0800

    typedef complete

--- getting_familiar/typedef.c
+++ getting_familiar/typedef.c
@@ -12,11 +12,30 @@ typedef struct {
 	int year;
 } Dates;
 
-//
-typedef char[10] Strings;
+//array of 10 pointers to char
+typedef char* Strings[10];
+
+//pointer to array of 6 char
+typedef char(*P)[6];
 
 int main()
 {
+	char ab[6]="abafs";
+	Strings a;
+	a[0]=ab;
+	while(*a[0]!='\0')
+	{
+		printf("%c\n",*a[0]++);
+	}
+	printf("\n");
+	P p=NULL;
+	p=&ab;
+	int i=0;
+	while((*p)[i]!='\0')
+	{
+		printf("%c\n",(*p)[i++]);
+	}
+
 	return 0;
 
 }
